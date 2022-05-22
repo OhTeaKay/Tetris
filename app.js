a9254437d60e56b36e0fc93f6fa32d161aca4b6f
Oskar Koźluk <s23220@pjwstk.edu.pl>
2022-05-22 12:40:58 +0200

    added freeze function

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
-    const ScoreDisplay = document.querySelector('#score')
-    const StartBtn = document.querySelector('#start-button')
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0;
+    let timerId
 
     const lTetromino = [
         [1, width+1, width*2+1, 2],
@@ -68,10 +70,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if(e.keyCode === 37){
             moveLeft()
         }else if(e.keyCode === 38){
-
+            rotate()
         }else if(e.keyCode === 39){
             moveRight()
-
         }else if(e.keyCode === 40){
             moveDown()
         }
@@ -86,13 +87,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function freeze(){
-        if(current.some(index => squares[currentPosition + index + width].
-            classList.contains('taken'))) {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
-            random = Math.floor(Math.random() * theTetrominoes.length)
+            random = nextRandom
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
             current = theTetrominoes[random][currentRotation]
             currentPosition = 4
             draw()
+            displayShape()
         }
     }
 
@@ -119,5 +121,50 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         draw()
     }
+
+    function rotate(){
+        undraw()
+        currentRotation ++
+        if(currentRotation == current.length){ //back to default
+            currentRotation = 0
+        }
+        current = theTetrominoes[random][currentRotation]
+        draw()
+    }
+
+//next
+    const displaySquares = document.querySelectorAll('.mini-grid div')
+    const displayWidth = 4;
+    let displayIndex = 0;
+
+
+    const upNextTetrominoes  = [
+        [1, displayWidth+1, displayWidth*2+1, 2],
+        [0, displayWidth, displayWidth+1, displayWidth*2+1],
+        [1, displayWidth, displayWidth+1, displayWidth+2],
+        [0,1, displayWidth, displayWidth+1],
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1]
+    ]
+
+    function displayShape() {
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+        })
+        upNextTetrominoes[nextRandom].forEach( index => {
+        displaySquares[displayIndex + index].classList.add('tetromino')
+        })
+    }
+
+    startBtn.addEventListener('click', () => {
+        if (timerId){
+            clearInterval(timerId)
+            timerId = null
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 1000)
+            nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+            displayShape()
+        }
+    })
 
 })
